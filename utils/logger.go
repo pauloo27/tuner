330a582fe761ee1efd1e5505e01b6e961400785a
Paulo <pauloeduardo2709@gmail.com>
2020-09-06 21:19:04 -0300

    Add white color

--- utils/logger.go
+++ utils/logger.go
@@ -8,6 +8,7 @@ const ColorRed = "\033[31m"
 const ColorGreen = "\033[32m"
 const ColorYellow = "\033[33m"
 const ColorBlue = "\033[34m"
+const ColorWhite = "\033[39m"
 
 func HandleError(err error, message string) {
 	if err != nil {
