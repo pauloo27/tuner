4c5bc3cf9b407098e21fa2ff182eb8030103bc2a
Paulo <pauloeduardo2709@gmail.com>
2020-12-11 20:58:26 -0300

    Add terminal.go with terminal stuff

--- utils/logger.go
+++ utils/logger.go
@@ -2,9 +2,6 @@ package utils
 
 import (
 	"log"
-	"os"
-	"syscall"
-	"unsafe"
 )
 
 const (
@@ -16,26 +13,6 @@ const (
 	ColorBlue   = "\033[34m"
 	ColorWhite  = "\033[39m"
 )
-
-type TerminalSize struct {
-	Row uint16
-	Col uint16
-}
-
-func GetTerminalWidth() *TerminalSize {
-	ws := &TerminalSize{}
-	retCode, _, err := syscall.Syscall(syscall.SYS_IOCTL,
-		uintptr(syscall.Stdin),
-		uintptr(syscall.TIOCGWINSZ),
-		uintptr(unsafe.Pointer(ws)))
-
-	if retCode != 0 {
-		HandleError(err, "Cannot get terminal size")
-		os.Exit(0)
-	}
-
-	return ws
-}
 
 func HandleError(err error, message string) {
 	if err != nil {
---
+++ utils/terminal.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+type TerminalSize struct {
+	Row uint16
+	Col uint16
+}
+
+func GetTerminalWidth() *TerminalSize {
+	ws := &TerminalSize{}
+	retCode, _, err := syscall.Syscall(syscall.SYS_IOCTL,
+		uintptr(syscall.Stdin),
+		uintptr(syscall.TIOCGWINSZ),
+		uintptr(unsafe.Pointer(ws)))
+
+	if retCode != 0 {
+		HandleError(err, "Cannot get terminal size")
+		os.Exit(0)
+	}
+
+	return ws
+}
