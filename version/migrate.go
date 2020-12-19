86904000d28c2dc59ebd8030d8640aed6921d19f
Paulo <pauloeduardo2709@gmail.com>
2020-12-19 16:17:12 -0300

    Improve Migrate() function

--- version/migrate.go
+++ version/migrate.go
@@ -4,11 +4,14 @@ import (
 	"strings"
 
 	"github.com/Pauloo27/tuner/player"
+	"github.com/Pauloo27/tuner/storage"
 )
 
 func Migrate(currentVersion string) {
 	if currentVersion != player.State.Data.Version {
 		switch strings.Split(currentVersion, "-")[0] {
 		}
+		player.State.Data.Version = currentVersion
+		storage.Save(player.State.Data)
 	}
 }
