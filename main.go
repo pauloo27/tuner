0d54105c4eb717e0dcecf22177c107cfba098ba2
Paulo <pauloeduardo2709@gmail.com>
2021-09-04 01:47:38 -0300

    feat: add simple tview screen
    [ci skip]

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
 const VERSION = "0.0.4-dev"
 
 func main() {
-	fmt.Println("Tuner version", VERSION)
+	flex := tview.NewFlex()
+
+	flex.SetBackgroundColor(tcell.ColorDefault)
+
+	textView := tview.NewTextView().SetText("Tuner - " + VERSION)
+	textView.SetBackgroundColor(tcell.ColorDefault)
+
+	flex.AddItem(textView, 0, 1, true)
+
+	if err := tview.NewApplication().SetRoot(flex, true).Run(); err != nil {
+		panic(err)
+	}
 }
