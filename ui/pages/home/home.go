eaa15f0b79d5bb32b937104fff20128da8c2d78d
Paulo <pauloeduardo2709@gmail.com>
2021-09-04 19:31:20 -0300

    feat: add simple home page

--- ui/pages/home/home.go
+++ ui/pages/home/home.go
@@ -2,10 +2,28 @@ package pages
 
 import (
 	"github.com/Pauloo27/tuner/ui"
-	"github.com/Pauloo27/tuner/ui/utils"
+	"github.com/Pauloo27/tuner/version"
+	"github.com/rivo/tview"
 )
 
 func init() {
-	container := utils.CreateContainer()
+	container := tview.NewGrid()
+	container.SetBackgroundColor(ui.GetTheme().PrimitiveBackgroundColor)
+	container.SetColumns(0)
+	container.SetRows(1, 0)
+
+	label := tview.NewTextView()
+	label.SetBackgroundColor(ui.GetTheme().PrimitiveBackgroundColor)
+	label.SetText("Tuner - " + version.Current)
+	label.SetTextAlign(tview.AlignCenter)
+
+	searchInput := tview.NewInputField()
+	searchInput.SetBackgroundColor(ui.GetTheme().PrimitiveBackgroundColor)
+	searchInput.SetFieldBackgroundColor(ui.GetTheme().PrimitiveBackgroundColor)
+	searchInput.SetLabel("Search for: ")
+
+	container.AddItem(label, 0, 0, 1, 1, 0, 0, false)
+	container.AddItem(searchInput, 1, 0, 1, 1, 0, 0, true)
+
 	ui.RegisterPage("home", container)
 }
--- ui/utils/factory.go
+++ ui/utils/factory.go
@@ -1,12 +1,3 @@
 package utils
 
-import (
-	"github.com/Pauloo27/tuner/ui"
-	"github.com/rivo/tview"
-)
-
-func CreateContainer() *tview.Flex {
-	container := tview.NewFlex()
-	container.SetBackgroundColor(ui.GetTheme().PrimitiveBackgroundColor)
-	return container
-}
+// TODO:
