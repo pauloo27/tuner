6d722a8410288a71b241d10934809cbdaa9f3537
Paulo <pauloeduardo2709@gmail.com>
2021-09-04 22:27:04 -0300

    feat: youtube search

--- internal/providers/search/search.go
+++ internal/providers/search/search.go
@@ -1,20 +1,26 @@
 package search
 
+import "time"
+
 type SearchResult struct {
-	Artist, Title, Length string
-	IsLive                bool
+	Artist, Title string
+	IsLive        bool
+	Length        time.Duration
 }
 
 type SearchProvider interface {
 	GetName() (name string)
-	SearchFor(searchQuery string) []*SearchResult
+	SearchFor(searchQuery string) (results []*SearchResult, err error)
 }
 
 var searchProviders []SearchProvider
 
-func SearchInAll(query string) (results []*SearchResult) {
+func SearchInAll(query string) (results []*SearchResult, err error) {
 	for _, provider := range searchProviders {
-		r := provider.SearchFor(query)
+		r, err := provider.SearchFor(query)
+		if err != nil {
+			return nil, err
+		}
 		results = append(results, r...)
 	}
 	return
--- internal/providers/search/youtube.go
+++ internal/providers/search/youtube.go
@@ -1,5 +1,11 @@
 package search
 
+import (
+	"time"
+
+	"github.com/Pauloo27/searchtube"
+)
+
 type YouTubeSearch struct {
 }
 
@@ -9,7 +15,24 @@ func (YouTubeSearch) GetName() (name string) {
 	return "YouTube"
 }
 
-func (YouTubeSearch) SearchFor(searchQuery string) []*SearchResult {
-	// TODO:
-	return nil
+func init() {
+	searchProviders = append(searchProviders, &YouTubeSearch{})
+}
+
+func (YouTubeSearch) SearchFor(searchQuery string) ([]*SearchResult, error) {
+	youtubeResults, err := searchtube.Search(searchQuery, 10)
+	if err != nil {
+		return nil, err
+	}
+	var results []*SearchResult
+	for _, result := range youtubeResults {
+		results = append(results, &SearchResult{
+			Artist: result.Uploader,
+			Title:  result.Title,
+			IsLive: result.Live,
+			// TODO:
+			Length: time.Duration(0),
+		})
+	}
+	return results, nil
 }
