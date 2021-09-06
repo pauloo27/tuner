69c44c3843d5914b7e6f2574c7ee882936128747
Paulo <pauloeduardo2709@gmail.com>
2021-09-05 21:07:04 -0300

    test: add tests to youtube search provider [WIP]
    [ci skip]

---
+++ internal/providers/search/youtube_test.go
@@ -0,0 +1,32 @@
+package search_test
+
+import (
+	"testing"
+
+	"github.com/Pauloo27/tuner/internal/providers/search"
+	"github.com/stretchr/testify/assert"
+)
+
+var youtubeProvider search.YouTubeSearch
+
+func TestYouTubeSearch(t *testing.T) {
+	assert.Equal(t, "YouTube", youtubeProvider.GetName())
+	results, err := youtubeProvider.SearchFor("no copyright songs")
+	assert.Nil(t, err)
+	assert.NotNil(t, results)
+	assert.NotEmpty(t, results)
+	assert.Len(t, results, 10)
+}
+
+func TestSearchResult(t *testing.T) {
+	results, err := youtubeProvider.SearchFor("https://www.youtube.com/watch?v=yJg-Y5byMMw")
+	assert.Nil(t, err)
+	assert.NotNil(t, results)
+	assert.NotEmpty(t, results)
+	assert.GreaterOrEqual(t, len(results), 1)
+	result := results[0]
+	assert.Equal(t, "NoCopyrightSounds", result.Artist)
+	assert.Equal(t, "Warriyo - Mortals (feat. Laura Brehm) [NCS Release]", result.Title)
+	assert.False(t, result.IsLive)
+	assert.Equal(t, "3m50s", result.Length.String())
+}
