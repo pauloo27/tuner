7c9a747c4c34a1d8662bd2a9937df3a26f758a26
Paulo <pauloeduardo2709@gmail.com>
2021-09-06 18:40:58 -0300

    improve: searching page code

--- internal/ui/pages/searching/searching.go
+++ internal/ui/pages/searching/searching.go
@@ -10,18 +10,54 @@ import (
 	"github.com/rivo/tview"
 )
 
+var resultList *tview.List
+var label *tview.TextView
+
+func onStart(params ...interface{}) {
+	searchQuery := params[0].(string)
+	resultList.Clear()
+	label.SetText(utils.Fmt("Searching for %s...", searchQuery))
+	go func() {
+		results, err := search.SearchInAll(searchQuery)
+		ui.App.QueueUpdateDraw(func() {
+			if err != nil {
+				label.SetText("Something went wrong =(")
+			}
+			label.SetText(utils.Fmt("Results for %s:", searchQuery))
+			for i, result := range results {
+				// limit results to 10
+				if i == 10 {
+					break
+				}
+				shortcut := strconv.Itoa(i + 1)
+				details := utils.Fmt("By [white]%s [green] - [white]%s[green]", result.Artist, result.Length)
+				// TODO: escape colors?
+				resultList.AddItem(
+					result.Title, details, rune(shortcut[len(shortcut)-1]), func() {
+						ui.SwitchPage("player", result.URL)
+					},
+				)
+			}
+			resultList.AddItem("Cancel", "Press c to cancel", 'c', func() {
+				ui.SwitchPage("home")
+			})
+		})
+	}()
+}
+
 func init() {
 	container := tview.NewGrid()
 	container.SetColumns(0)
 	container.SetRows(1, 0)
 	container.SetBackgroundColor(tcell.ColorDefault)
 
-	label := tview.NewTextView()
+	label = tview.NewTextView()
 	label.SetTextAlign(tview.AlignCenter)
 
-	resultList := tview.NewList()
+	resultList = tview.NewList()
 	resultList.SetSelectedBackgroundColor(tcell.ColorGreen)
 	resultList.SetSelectedTextColor(tcell.ColorBlack)
+	// vim-like keybinds (k and j navigation)
 	resultList.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Rune() {
 		case 'k':
@@ -37,36 +73,6 @@ func init() {
 
 	ui.RegisterPage(&ui.Page{
 		Name: "searching", Container: container,
-		OnStart: func(params ...interface{}) {
-			searchQuery := params[0].(string)
-			resultList.Clear()
-			label.SetText(utils.Fmt("Searching for %s...", searchQuery))
-			go func() {
-				results, err := search.SearchInAll(searchQuery)
-				ui.App.QueueUpdateDraw(func() {
-					if err != nil {
-						label.SetText("Something went wrong =(")
-					}
-					label.SetText(utils.Fmt("Results for %s:", searchQuery))
-					for i, result := range results {
-						// limit results to 10
-						if i == 10 {
-							break
-						}
-						shortcut := strconv.Itoa(i + 1)
-						details := utils.Fmt("By [white]%s [green] - [white]%s[green]", result.Artist, result.Length)
-						// TODO: escape colors?
-						resultList.AddItem(
-							result.Title, details, rune(shortcut[len(shortcut)-1]), func() {
-								ui.SwitchPage("player", result.URL)
-							},
-						)
-					}
-					resultList.AddItem("Cancel", "Press c to cancel", 'c', func() {
-						ui.SwitchPage("home")
-					})
-				})
-			}()
-		},
+		OnStart: onStart,
 	})
 }
