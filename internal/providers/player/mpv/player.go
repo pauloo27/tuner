51777727b3cfe2b7f09dd783793b6c5bf9d53b61
Paulo <git@pauloo27.dev>
2021-10-30 16:47:56 -0300

    feat: add simple Play() implementation based on mpv

--- internal/providers/player/mpv/player.go
+++ internal/providers/player/mpv/player.go
@@ -1,3 +1,51 @@
 package mpv
 
+import (
+	"github.com/Pauloo27/tuner/internal/providers/player"
+	"github.com/Pauloo27/tuner/internal/providers/player/mpv/libmpv"
+	"github.com/Pauloo27/tuner/internal/providers/source"
+)
+
 // TODO:
+type MpvPlayer struct {
+	Instance *libmpv.Mpv
+}
+
+var _ player.PlayerProvider = MpvPlayer{}
+
+func (p MpvPlayer) Play(result *source.SearchResult) error {
+	p.Instance.Command([]string{"loadfile", result.URL})
+	return nil
+}
+
+func (MpvPlayer) GetName() string {
+	return "MPV"
+}
+
+func newMpvPlayer() (*MpvPlayer, error) {
+	instance := libmpv.Create()
+
+	mustSetOption := func(name string, data string) {
+		err := instance.SetOptionString(name, data)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	mustSetOption("video", "no")
+	mustSetOption("cache", "no")
+
+	err := instance.Initialize()
+
+	return &MpvPlayer{
+		instance,
+	}, err
+}
+
+func init() {
+	mpv, err := newMpvPlayer()
+	if err != nil {
+		panic(err)
+	}
+	player.DefaultProvider = mpv
+}
