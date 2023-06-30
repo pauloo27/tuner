19e7c704479b0bb9a27cccc4beb108dfb89578fb
Paulo <git@pauloo27.dev>
2023-06-30 11:12:48 -0300

    feat: update debian install script
    - remove youtube-dl
    - use go with apt

--- install-debian.sh
+++ install-debian.sh
@@ -17,17 +17,8 @@ $NO_FORMAT
 echo "Your system info:"
 cat /etc/os-release
 
-echo "Installing libmpv-dev, wget, python, git, make, and the Icon Font"
-sudo apt-get install fonts-font-awesome libmpv-dev wget git make python3 python3-pip --no-install-recommends
-
-echo "Installing GoLang 1.17"
-wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
-sudo tar -zxvf go1.17.linux-amd64.tar.gz -C /usr/local/
-echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee /etc/profile.d/go.sh
-source /etc/profile.d/go.sh
-
-echo "Installing youtube-dl"
-sudo -H pip3 install --upgrade youtube-dl
+echo "Installing libmpv-dev, wget, golang, git, make, and the Icon Font"
+sudo apt-get install libmpv-dev wget golang git make fonts-font-awesome --no-install-recommends
 
 echo "Cloning Tuner..."
 git clone https://github.com/Pauloo27/tuner.git
