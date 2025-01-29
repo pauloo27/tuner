80ff835dc83275e247ce65bd41d1fbcc30034017
Paulo <git@pauloo27.dev>
2025-01-29 20:34:58 -0300

    ref: solve a problem but create another one

--- internal/ui/view/root/model.go
+++ internal/ui/view/root/model.go
@@ -8,9 +8,7 @@ type model struct {
 	searchView tea.Model
 	debugView  tea.Model
 
-	// yeah i know i should avoid pointers and etc, but happens
-	activeView   *tea.Model
-	previousView *tea.Model
+	isInDebug bool
 }
 
 func NewModel(
@@ -20,8 +18,6 @@ func NewModel(
 	return model{
 		searchView: searchModel,
 		debugView:  debugModel,
-
-		activeView: &searchModel,
 	}
 }
 
@@ -32,29 +28,34 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC:
 			return m, tea.Quit
 		case tea.KeyCtrlD:
-			if *m.activeView == m.debugView {
-				m.activeView = m.previousView
-			} else {
-				m.previousView = m.activeView
-				m.activeView = &m.debugView
-			}
+			m.isInDebug = !m.isInDebug
 			// ensure the activeView knows its now visible
 			cmd = visible
 		}
 	}
 
-	var activeViewCmd tea.Cmd
-	*m.activeView, activeViewCmd = (*m.activeView).Update(msg)
+	var viewCmd tea.Cmd
 
-	return m, tea.Batch(cmd, activeViewCmd)
+	if m.isInDebug {
+		m.debugView, viewCmd = m.debugView.Update(msg)
+	} else {
+		m.searchView, viewCmd = m.searchView.Update(msg)
+	}
+
+	return m, tea.Batch(cmd, viewCmd)
 }
 
 func (m model) View() string {
-	return (*m.activeView).View()
+	if m.isInDebug {
+		return m.debugView.View()
+	}
+
+	return m.searchView.View()
 }
